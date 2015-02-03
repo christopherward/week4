d9a839d788e94979a3b306048404b5f8882f8785
christopherward <christopher.ward5@gmail.com>
2015-02-02 19:32:45 -0600

    HW 4 Update, 2.2.15
    Additional comments to describe structure and function of class
    BankAccount

--- banking.py
+++ banking.py
@@ -5,6 +5,7 @@
 
 class BankAccount:
 
+    #initialize constructor variables for account balances
     def __init__(self):
         self.my_checking_account = 0
         self.my_savings_account = 0
@@ -13,20 +14,24 @@ class BankAccount:
         return self.balance
 
     def deposit(self, deposit_amount):
+        #increement balance by deposit amount
         self.balance += deposit_amount
 
     def withdraw(self, withdraw_amount):
-        #offer overdraft protection
+        #decrement balance by withdrawal amount
+        #reject transaction if withdrawal amount is greater than balance
         if withdraw_amount <= self.balance:
             self.balance -= withdraw_amount
 
     def transfer(self, transfer_amount, to_account):
-        #overdraft protection in transactions
+        #account for overdraft protection in transactions
         if transfer_amount < self.balance:
             self.withdraw(transfer_amount)
             to_account.deposit(transfer_amount)
+        #if overdraft protection is invoked, do not transfer funds
         else:
             pass
 
+#initialize account variables by calling class BankAccount
 my_checking_account = BankAccount()
 my_savings_account = BankAccount()
